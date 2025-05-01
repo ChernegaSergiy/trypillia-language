610f80919cc98893fcfa6a32e9a8ff59427cd4ce
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-01 05:57:09 +0300

    Add files via upload

---
+++ src/parser/Parser.cpp
@@ -0,0 +1,57 @@
+﻿#include "Parser.h"
+#include <stdexcept>
+
+Parser::Parser(Lexer &lexer) : lexer(lexer) {
+    advance();
+}
+
+void Parser::advance() {
+    currentToken = lexer.nextToken();
+}
+
+void Parser::consume(TokenType type) {
+    if (currentToken.type == type) {
+        advance();
+    } else {
+        throw std::runtime_error("Unexpected token");
+    }
+}
+
+ASTNode* Parser::parse() {
+    std::vector<ASTNode*> declarations;
+    while (currentToken.type != TokenType::END_OF_FILE) {
+        declarations.push_back(declaration());
+    }
+    return new ProgramNode(declarations); // Need to define ProgramNode
+}
+
+ASTNode* Parser::expression() {
+    ASTNode* node = term();
+    while (currentToken.type == TokenType::PLUS || 
+           currentToken.type == TokenType::MINUS) {
+        Token op = currentToken;
+        advance();
+        node = new BinaryExpr(node, op, term());
+    }
+    return node;
+}
+
+ASTNode* Parser::declaration() {
+    // Example for parsing a function declaration
+    if (currentToken.type == TokenType::FN) {
+        return parseFunction();
+    }
+    // Handle other declarations
+    return nullptr;
+}
+
+ASTNode* Parser::parseFunction() {
+    consume(TokenType::FN);
+    // Parse function name, parameters, and body
+    return new FunctionNode(); // Placeholder
+}
+
+ASTNode* Parser::parseClass() {
+    // Implement class parsing logic
+    return nullptr;
+}
--- src/parser/Parser.h
+++ src/parser/Parser.h
@@ -1,1 +1,25 @@
+﻿#ifndef PARSER_H
+#define PARSER_H
 
+#include "Lexer.h"
+#include "AST.h"
+
+class Parser {
+public:
+    Parser(Lexer &lexer);
+    ASTNode* parse();
+
+private:
+    Lexer &lexer;
+    Token currentToken;
+
+    void advance();
+    void consume(TokenType type);
+    ASTNode* expression();
+    ASTNode* statement();
+    ASTNode* declaration();
+    ASTNode* parseFunction();
+    ASTNode* parseClass();
+};
+
+#endif // PARSER_H
