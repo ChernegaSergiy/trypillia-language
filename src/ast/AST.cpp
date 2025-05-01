57f78b218f1fd891b0984d627836bc0abad369aa
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-01 07:09:45 +0300

    feat(ast): Extend AST with full node hierarchy and visitor

---
+++ src/ast/AST.cpp
@@ -0,0 +1,59 @@
+﻿#include "AST.h"
+
+// Implementation of accept methods for each AST node type
+
+void ProgramNode::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void BinaryExpr::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void LiteralExpr::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void VariableExpr::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void AssignExpr::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void CallExpr::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void ExpressionStmt::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void PrintStmt::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void VarStmt::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void BlockStmt::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void IfStmt::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void WhileStmt::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void FunctionNode::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
+
+void ClassNode::accept(ASTVisitor* visitor) {
+    visitor->visit(this);
+}
