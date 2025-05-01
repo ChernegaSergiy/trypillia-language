ca5a887c47a3574517d589e5fcda735b1a285257
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-01 06:05:32 +0300

    feat(utils): Add ErrorHandling utility

--- src/utils/ErrorHandling.h
+++ src/utils/ErrorHandling.h
@@ -1,1 +1,11 @@
+﻿#ifndef ERROR_HANDLING_H
+#define ERROR_HANDLING_H
 
+#include <string>
+
+class ErrorHandling {
+public:
+    static void reportError(const std::string& message);
+};
+
+#endif // ERROR_HANDLING_H
