1b3227332ab1a37c4a3520d0768ca4b05c9bc44f
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-01 05:58:13 +0300

    Add files via upload

--- src/ast/AST.h
+++ src/ast/AST.h
@@ -1,1 +1,35 @@
+﻿#ifndef AST_H
+#define AST_H
 
+#include <vector>
+#include <string>
+
+class ASTNode {
+public:
+    virtual ~ASTNode() = default;
+};
+
+class ExprNode : public ASTNode {};
+class StmtNode : public ASTNode {};
+
+class BinaryExpr : public ExprNode {
+public:
+    ExprNode* left;
+    Token op;
+    ExprNode* right;
+};
+
+class FunctionNode : public StmtNode {
+public:
+    std::string name;
+    std::vector<std::string> params;
+    std::vector<StmtNode*> body;
+};
+
+class ClassNode : public StmtNode {
+public:
+    std::string name;
+    std::vector<FunctionNode*> methods;
+};
+
+#endif // AST_H
