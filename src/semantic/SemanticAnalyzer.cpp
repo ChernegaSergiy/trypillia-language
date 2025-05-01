a524800f13bf1e8b540b8bb04f99e6047538562a
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-01 05:59:47 +0300

    Add files via upload

---
+++ src/semantic/SemanticAnalyzer.cpp
@@ -0,0 +1,23 @@
+﻿#include "SemanticAnalyzer.h"
+#include <stdexcept>
+
+void SemanticAnalyzer::analyze(ASTNode* ast) {
+    SymbolTable globalScope;
+    
+    class Visitor : public ASTVisitor {
+        SymbolTable* currentScope;
+        
+        void visit(FunctionNode* node) override {
+            currentScope = new SymbolTable(currentScope);
+            // Process parameters
+            for (auto stmt : node->body) {
+                stmt->accept(this);
+            }
+            currentScope = currentScope->parent;
+        }
+        
+        // Other visit methods...
+    };
+    
+    Visitor().visit(ast);
+}
--- src/semantic/SemanticAnalyzer.h
+++ src/semantic/SemanticAnalyzer.h
@@ -1,1 +1,11 @@
+﻿#ifndef SEMANTIC_ANALYZER_H
+#define SEMANTIC_ANALYZER_H
 
+#include "AST.h"
+
+class SemanticAnalyzer {
+public:
+    void analyze(ASTNode* ast);
+};
+
+#endif // SEMANTIC_ANALYZER_H
