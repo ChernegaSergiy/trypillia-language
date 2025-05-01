35f436d79bc090aa4d1a1b1aba0ed936d1f54d8f
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-01 06:01:20 +0300

    Add files via upload

---
+++ src/codegen/CodeGenerator.cpp
@@ -0,0 +1,7 @@
+﻿#include "CodeGenerator.h"
+
+void CodeGenerator::generate(ASTNode* ast) {
+    // Implement code generation logic
+    if (!ast) return;
+    // Generate code for different AST nodes
+}
