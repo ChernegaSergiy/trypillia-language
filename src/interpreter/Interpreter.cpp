d4a4644fb2def567d553e86515a7ff56e07bffad
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-05-01 06:02:27 +0300

    feat(interpreter): Add interpreter skeleton

---
+++ src/interpreter/Interpreter.cpp
@@ -0,0 +1,7 @@
+﻿#include "Interpreter.h"
+
+void Interpreter::execute(ASTNode* ast) {
+    // Implement execution logic
+    if (!ast) return;
+    // Execute different AST nodes
+}
--- src/interpreter/Interpreter.h
+++ src/interpreter/Interpreter.h
@@ -1,1 +1,11 @@
+﻿#ifndef INTERPRETER_H
+#define INTERPRETER_H
 
+#include "AST.h"
+
+class Interpreter {
+public:
+    void execute(ASTNode* ast);
+};
+
+#endif // INTERPRETER_H
